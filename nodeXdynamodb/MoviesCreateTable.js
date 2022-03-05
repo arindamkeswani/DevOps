c0a21162ea5a57b9297b2cd24f7ff4bd0c249f23
Arindam <arindamkeswani@gmail.com>
2022-03-05 16:26:23 +0530

    Step 1: Creating a table

---
+++ nodeXdynamodb/MoviesCreateTable.js
@@ -0,0 +1,42 @@
+var AWS = require("aws-sdk");
+require('dotenv').config();
+
+//Use the commented lines if you are using DynamoDB locally
+// AWS.config.update({
+//     region: "us-east-1",
+//     endpoint: "http://localhost:8000"
+// });
+
+
+AWS.config.update({
+    region: "us-east-1",
+    endpoint: "https://dynamodb.us-east-1.amazonaws.com"});
+
+var dynamodb = new AWS.DynamoDB();
+
+//Defining table Schema and Provisioning Units
+var params = {
+    TableName : "Movies",
+    KeySchema: [       
+        { AttributeName: "year", KeyType: "HASH"},  //Partition key
+        { AttributeName: "title", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "year", AttributeType: "N" },
+        { AttributeName: "title", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+
+//Creating the table
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
