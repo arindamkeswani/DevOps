832e013d1f257da453c545c08c80669cb5991ca9
Arindam <arindamkeswani@gmail.com>
2022-03-09 19:06:27 +0530

    1. Terraform overview | Launch EC2 Instance

---
+++ terraform/project1/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+# Create a VPC
+resource "aws_instance" "my-first-server" {
+  ami = "ami-0e472ba40eb589f49"
+
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform_project_1_sample"
+  }
+}
