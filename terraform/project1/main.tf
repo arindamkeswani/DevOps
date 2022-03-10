af8617b03bdc8badf0cd68d1c27056ffd767445d
Arindam <arindamkeswani@gmail.com>
2022-03-10 13:05:42 +0530

    4. Referencing resources

--- terraform/project1/main.tf
+++ terraform/project1/main.tf
@@ -23,3 +23,21 @@ provider "aws" {
 #     Name = "terraform_project_1_sample_name_change"
 #   }
 # }
+
+#Create a VPC
+resource "aws_vpc" "first-terraform-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags={
+      Name = "production-vpc"
+  }
+}
+
+#Subnet
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-terraform-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
