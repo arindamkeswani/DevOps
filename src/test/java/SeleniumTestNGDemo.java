6e7305923b8f9387ac0a7cce6701a243f032e907
Arindam <arindamkeswani@gmail.com>
2022-03-26 13:38:16 +0530

    Extent Reports added

--- src/test/java/SeleniumTestNGDemo.java
+++ src/test/java/SeleniumTestNGDemo.java
@@ -4,10 +4,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+
 public class SeleniumTestNGDemo {
 
     @Test
     public void sampleTestMethod() throws InterruptedException {
+        ExtentReports extent = new ExtentReports();
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+        spark.config().setTheme(Theme.DARK);
+        spark.config().setDocumentTitle("Test Report");
+        extent.attachReporter(spark);
+
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://demo.automationtesting.in");
@@ -15,5 +28,12 @@ public class SeleniumTestNGDemo {
         driver.findElement(By.id("enterimg")).click(); //click on go button
         Thread.sleep(3001);
         driver.quit();
+
+        ExtentTest test = extent.createTest("Login test");
+        test.pass("Login successful");
+        test.info("Details entered");
+        test.pass("Logged in");
+
+        extent.flush(); //Unless you call this method, report will not have logs
     }
 }
